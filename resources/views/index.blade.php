dac69a2433435364d6faf58dc87ea9318f0dc1a8
Mr. Cryptoholic <mrcryptoholic@gmail.com>
2023-03-12 04:18:52 +0100

    Making logo and margins smaller

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,8 +22,8 @@
                     <img
                         src="{{ url('twitter-logo-white.png') }}"
                         alt="Twitter Score Logo White"
-                        width="150px"
-                        height="150px"
+                        width="130px"
+                        height="130px"
                     >
                 </div>
                 <div class="flex justify-center">
@@ -85,7 +85,7 @@
                     </div>
                 </div>
 
-                <div class="mt-16">
+                <div class="mt-6">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
